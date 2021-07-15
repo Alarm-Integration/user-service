a779d18fe2229ff06bdcd0c35b485abc4b14cc97
Dexter(노영삼) <nys@gabia.com>
2021-07-15 13:55:08 +0900

    cicd test2

--- src/test/java/com/gabia/avengers/userservice/repository/UserRepositoryTest.java
+++ src/test/java/com/gabia/avengers/userservice/repository/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @Transactional
