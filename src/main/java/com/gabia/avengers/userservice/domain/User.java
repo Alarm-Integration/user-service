c124201636d001e0ceeaec298631c147adbfefe5
Dexter(노영삼) <nys@gabia.com>
2021-07-19 11:15:28 +0900

    feat: User 엔티티의 생성일 수정일 추가 - JPA Auditing 사용 BaseEntity.java 작성

--- src/main/java/com/gabia/avengers/userservice/domain/User.java
+++ src/main/java/com/gabia/avengers/userservice/domain/User.java
@@ -1,19 +1,24 @@
 package com.gabia.avengers.userservice.domain;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@EqualsAndHashCode
 @Getter
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @Table(name = "users")
 @Entity
-public class User {
-    @Id
+public class User extends BaseEntity{
+
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
 
     @NotBlank
